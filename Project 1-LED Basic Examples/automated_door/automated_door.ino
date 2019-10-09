aff91bb88bc6ae7d00829f47a281d0a7a3274a23
Mohankumar M C <49070974+MohankumarMC@users.noreply.github.com>
2019-10-09 23:11:23 +0530

    Add files via upload

---
+++ Project 1-LED Basic Examples/automated_door/automated_door.ino
@@ -0,0 +1,13 @@
+void setup() {
+  
+pinMode (13, OUTPUT);
+pinMode (7, INPUT);
+}
+void loop() {
+ if (digitalRead(7))
+  {
+   digitalWrite (13,HIGH);
+   delay (500);
+    digitalWrite (13,LOW);
+  }
+}
